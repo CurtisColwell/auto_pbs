fa529a3785a8ea44f77acc3ddd1e46860662a65c
CurtisColwell <ccolwell@talapas-ln2.cm.cluster>
2019-11-01 15:30:39 -0700

    Made file extension for input file general

--- gaussian/scripts/submit_gaussian.bash
+++ gaussian/scripts/submit_gaussian.bash
@@ -28,9 +28,6 @@ main() {
 
 	read_args "$@"
 
-	if [ -z "$ARG_NO_UPDATE" ]; then
-		update_repo
-	fi
 	check_for_jobs
 }
 
@@ -46,16 +43,6 @@ read_args()
 				PARTITION="$arg";;
 		esac
 	done
-}
-
-update_repo() {
-	cd "$SCRIPTS_DIR"
-	git checkout -q master
-	PULL_RESULT=$(git pull)
-	if [ ! "$PULL_RESULT" == "Already up-to-date." ]; then
-		echo "Scripts have been updated, please run the script again."
-		exit
-	fi
 }
 
 check_for_jobs()
